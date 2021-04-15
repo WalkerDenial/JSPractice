9d35c38106e18ae8339c49a3d42716992eeded7d
Walker Denial <WalkerDenial@qq.com>
2021-04-15 09:14:46 +0800

    add bind method

--- src/js/base/Scope/call.index.js
+++ src/js/base/Scope/call.index.js
@@ -9,3 +9,8 @@ console.log(fn(10, 10));
 
 console.log(fn.call(object, 10, 10));
 console.log(fn.apply(object, [10, 10]));
+
+var _fn = fn.bind(object, 1, 2);
+console.log(_fn === fn);
+console.log(_fn());
+console.log(_fn(1, 4));
